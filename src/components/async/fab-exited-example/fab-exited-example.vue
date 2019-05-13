07642d62f247bedcee39f9d8add8b0f5b0c618f4
huangyu <fonigle@163.com>
2019-05-13 16:32:17 +0800

    add async component fab-exicted-example.

---
+++ src/components/async/fab-exited-example/fab-exited-example.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="fab-exited-example">
+        <mdc-fab icon="icon-heart"
+                 :exited="exited"></mdc-fab>
+        <mdc-button @click="exited=!exited" >切换</mdc-button>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                exited: false
+            }
+        }
+    }
+</script>
--- src/components/async/index.ts
+++ src/components/async/index.ts
@@ -2,4 +2,5 @@ import Vue from 'vue';
 
 
 Vue.component('start-example', () => import('./start-example/start-example.vue'));
-Vue.component('icon-button-toggle-example', () => import('./icon-button-toggle-example/icon-button-toggle-example.vue'))
+Vue.component('icon-button-toggle-example', () => import('./icon-button-toggle-example/icon-button-toggle-example.vue'));
+Vue.component('fab-exited-example', () => import('./fab-exited-example/fab-exited-example.vue'));
