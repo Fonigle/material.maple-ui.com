2f60e913fe981501be1296726cde4c41d383b373
huangyu <fonigle@163.com>
2019-05-14 09:28:43 +0800

    add async component: radio-value-example.

--- src/components/async/index.ts
+++ src/components/async/index.ts
@@ -5,3 +5,4 @@ Vue.component('start-example', () => import('./start-example/start-example.vue')
 Vue.component('icon-button-toggle-example', () => import('./icon-button-toggle-example/icon-button-toggle-example.vue'));
 Vue.component('fab-exited-example', () => import('./fab-exited-example/fab-exited-example.vue'));
 Vue.component('checkbox-value-example', () => import('./checkbox-value-example/checkbox-value-example.vue'));
+Vue.component('radio-value-example', () => import('./radio-value-example/radio-value-example.vue'));
---
+++ src/components/async/radio-value-example/radio-value-example.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="radio-value-example">
+        <mdc-radio group="fruit"
+                   value="apple"
+                   v-model="eat"></mdc-radio>
+        <mdc-radio group="fruit"
+                   value="banana"
+                   v-model="eat"></mdc-radio>
+        <mdc-radio group="fruit"
+                   value="cherry"
+                   v-model="eat"></mdc-radio>
+        <div class="bind-value">
+            绑定值：{{test1}}
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                eat: 'banana'
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    .radio-value-example {
+        display: flex;
+        align-items: center;
+        .bind-value {
+            margin-left: 32px;
+        }
+    }
+</style>
